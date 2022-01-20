6491488b7c5548846885360d406b2dba358889c7
titodelerinofilho <titodelerino@yahoo.com.br>
2022-01-20 14:52:02 -0300

    criaçao de novas rotas.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/cadastro/novo', [EventController::class, 'cadastro']);
+Route::get('/login', [EventController::class, 'login']);
